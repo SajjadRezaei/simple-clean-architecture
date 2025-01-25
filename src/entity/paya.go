4a26114cad7e48db73811c387ca706753a459f59
sajjad <sajjad@sajjads-MacBook-Pro.local>
2025-01-25 14:16:58 +0330

    # refactor code

--- src/entity/paya.go
+++ src/entity/paya.go
@@ -1,8 +1,6 @@
 package entity
 
 import (
-	"errors"
-	"regexp"
 	"time"
 )
 
@@ -14,17 +12,4 @@ type Paya struct {
 	Note      string    `json:"note"`
 	Status    string    `json:"status"` // pending, confirmed, canceled
 	CreatedAt time.Time `json:"createdAt"`
-}
-
-func (s *Paya) Validate() error {
-	if s.Price <= 0 {
-		return errors.New("price must be greater than zero")
-	}
-
-	shebaRegex := `^IR\d{24}$`
-	if !regexp.MustCompile(shebaRegex).MatchString(s.FromSheba) || !regexp.MustCompile(shebaRegex).MatchString(s.ToSheba) {
-		return errors.New("invalid sheba number format")
-	}
-
-	return nil
 }
